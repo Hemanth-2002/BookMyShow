178ad3e49fee220e48769fa6144cb47de4823525
Hemanth-2002 <hemanthkakumanu1@gmail.com>
2022-12-17 14:31:48 +0530

    clean ups

--- Mail/mail.go
+++ Mail/mail.go
@@ -1,7 +1,7 @@
 package Mail
 
 import (
-	"bms/model"
+	model "bms/model"
 	"fmt"
 	"net/http"
 	"strings"
@@ -27,20 +27,25 @@ func Mail(amount string, email string, coupon string, mode string) {
 	    "content": [
 	        {
 	            "type": "text/plain",
-	            "value":`+` "Your payment is successful !!!\nThe following are the payment details\nAmount: `+fmt.Sprint(amount)+`\nDiscountCouponId: `+fmt.Sprint(coupon)+`\nMode: `+`%s"`+`
+	            "value":`+` "Your payment is successful !!!\n\nThe following are the payment details:\n\nAmount: `+fmt.Sprint(amount)+`\n\nDiscount Coupon Number: `+fmt.Sprint(coupon)+`\n\nPayment Mode: `+fmt.Sprint(mode)+`\n\nThank You For Booking With Us"`+`
 			}
 	    ]
-	}`, email, mode)
+	}`, email)
+	fmt.Println(code)
 
 	payload := strings.NewReader(code)
 	req, _ := http.NewRequest("POST", url, payload)
 
 	req.Header.Add("content-type", "application/json")
-	req.Header.Add("X-RapidAPI-Key", "285bd11398msh3a5f0f9c659453fp128fd7jsn73c2bb09d864")
+	req.Header.Add("X-RapidAPI-Key", "81087b3865mshb8974e3c74f5b9dp1999ffjsn823050f51b45")
 	req.Header.Add("X-RapidAPI-Host", "rapidprod-sendgrid-v1.p.rapidapi.com")
 
 	res, err := http.DefaultClient.Do(req)
 	model.CheckError(err)
 	defer res.Body.Close()
+	// body, _ := io.ReadAll(res.Body)
+
+	// fmt.Println(res)
+	// fmt.Println(string(body))
 
 }
